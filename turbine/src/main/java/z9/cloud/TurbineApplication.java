cb5d3ea2385720f62baa16da039df89093d5a9fb
David Shue <david.shue@babycenter.com>
2016-03-17 15:51:23 -0700

    added turbine

--- turbine/src/main/java/z9/cloud/TurbineApplication.java
+++ turbine/src/main/java/z9/cloud/TurbineApplication.java
@@ -2,8 +2,12 @@ package z9.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
+@EnableTurbine
+@EnableHystrixDashboard
 public class TurbineApplication {
 
     public static void main(String[] args) {
