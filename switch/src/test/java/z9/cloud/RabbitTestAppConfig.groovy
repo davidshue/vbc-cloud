d3c99a450262aa4c6ef53b98b84943e65a7b6e26
David Shue <davidcshue@gmail.com>
2017-02-06 20:24:04 -0800

    fixed unit tests

--- switch/src/test/java/z9/cloud/RabbitTestAppConfig.groovy
+++ switch/src/test/java/z9/cloud/RabbitTestAppConfig.groovy
@@ -3,23 +3,21 @@ package z9.cloud
 import org.springframework.amqp.core.BindingBuilder
 import org.springframework.amqp.core.FanoutExchange
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory
+import org.springframework.amqp.rabbit.connection.ConnectionFactory
 import org.springframework.amqp.rabbit.core.RabbitTemplate
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class RabbitTestAppConfig {
 	@Bean
-	connectionFactory(@Value('${spring.rabbitmq.addresses}') String addresses,
-					  @Value('${spring.rabbitmq.username}') String username,
-					  @Value('${spring.rabbitmq.password}') String password) {
-		new CachingConnectionFactory(addresses: addresses, username:username, password:password)
+	ConnectionFactory connectionFactory() {
+		new CachingConnectionFactory(addresses: 'localhost', username:'guest', password:'guest')
 	}
-	
+
 	@Bean
-	rabbitTemplate() {
-		new RabbitTemplate(connectionFactory())
+	rabbitTemplate(ConnectionFactory factory) {
+		new RabbitTemplate(factory)
 	}
 
 	@Bean
