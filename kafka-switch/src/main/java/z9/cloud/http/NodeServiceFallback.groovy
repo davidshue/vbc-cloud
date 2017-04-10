35b0e4a62e747071e99e4e8149f37e0f219cdcc2
David Shue <davidcshue@gmail.com>
2017-04-10 09:29:31 -0700

    compilation errors

--- kafka-switch/src/main/java/z9/cloud/http/NodeServiceFallback.groovy
+++ kafka-switch/src/main/java/z9/cloud/http/NodeServiceFallback.groovy
@@ -1,5 +1,6 @@
 package z9.cloud.http
 
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Component
 import z9.cloud.core2.Input
 import z9.cloud.core2.Output
@@ -15,17 +16,17 @@ import z9.cloud.core2.Z9StatusLine
 @Component
 class NodeServiceFallback implements NodeService {
 	@Override
-	String v1() {
+	ResponseEntity<String> v1() {
 		return 'v1 failed'
 	}
 
 	@Override
-	Output testV1(Input input) {
+	ResponseEntity<Output> testV1(Input input) {
 		return new Output(code: 500, output: 'failed')
 	}
 
 	@Override
-	Z9HttpResponse httpV1(Z9HttpRequest input) {
+	ResponseEntity<Z9HttpResponse> httpV1(Z9HttpRequest input) {
 		return new Z9HttpResponse(
 				statusLine: new Z9StatusLine(protocolVersion: new Z9ProtocolVersion(protocol: 'https', major: 1, minor: 1),
 						statusCode: 200),
